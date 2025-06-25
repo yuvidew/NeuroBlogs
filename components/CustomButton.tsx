f5c5bafe2d446c47e963fc928cc3be3020d629f2
Yuvi dew <yuvraj@truelink.ai>
2025-06-25 07:12:42 +0530

    Add loading animation to CustomButton component for enhanced user feedback

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,6 +1,7 @@
-import { Text, TouchableOpacity } from 'react-native'
-import React from 'react';
+import { Animated, Image, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect, useRef } from 'react';
 import { ButtonProps } from '@/types/type';
+import { icons } from '@/constants/icons';
 
 
 /**
@@ -72,17 +73,49 @@ const CustomButton = ({
     IconLeft,
     IconRight,
     className,
+    loading = false,
     ...props
 }: ButtonProps) => {
+    const rotateAnim = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        if (loading) {
+            Animated.loop(
+                Animated.timing(rotateAnim, {
+                    toValue: 1,
+                    duration: 1000,
+                    useNativeDriver: true,
+                })
+            ).start();
+        } else {
+            rotateAnim.stopAnimation();
+        }
+    }, [loading]);
+
+    const rotateInterpolate = rotateAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
     return (
         <TouchableOpacity
             onPress={onPress}
             className={` p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 h-16 ${getBgVariantStyle(bgVariant)} ${className}`}
             {...props}
         >
-            {IconLeft}
-            <Text className={`text-lg font-JakartaBold ${getTextVariantStyle(textVariant)}`} >{title}</Text>
-            {IconRight && <IconRight />}
+            {loading ?
+                <Animated.Image
+                    source={icons.loader}
+                    style={[{ transform: [{ rotate: rotateInterpolate }] }]}
+                    tintColor="#fff"
+                    className={"size-6"}
+                />
+                :
+                <>
+                    {IconLeft}
+                    <Text className={`text-lg font-JakartaBold ${getTextVariantStyle(textVariant)}`} >{title}</Text>
+                    {IconRight && <IconRight />}
+                </>
+            }
         </TouchableOpacity>
     )
 }
